79bb3a80d1a26a1455e66df8a7f6d072ffe69e03
Dherik <dherikb@yahoo.com.br>
2024-01-21 17:30:57 -0300

    Adjusts

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dherik/ddd-golang-project/internal/app"
 	"github.com/dherik/ddd-golang-project/internal/config"
-	"github.com/dherik/ddd-golang-project/internal/domain"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
 )
 
 func main() {
@@ -35,18 +34,9 @@ func main() {
 
 	router := chi.NewRouter()
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("root."))
-	})
-
-	router.Route("/tasks", func(r chi.Router) {
-		r.Get("/", searchTasks)
-		r.Route("/{taskId}", func(r chi.Router) {
-			// r.Use(TaskCtx)
-			r.Get("/", GetTask)
-			// r.Delete("/", DeleteTask)
-		})
-	})
+	// r.Use(TaskCtx)
+	// r.Delete("/", DeleteTask)
+	app.SetupHandler(router)
 
 	http.ListenAndServe(":3333", router)
 
@@ -64,38 +54,11 @@ func main() {
 	// }
 }
 
-func searchTasks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("tasks"))
-}
-
-func GetTask(w http.ResponseWriter, r *http.Request) {
-	// article := r.Context().Value("article").(*Article)
-
-	task := domain.Task{}
-
-	if err := render.Render(w, r, NewTaskResponse(&task)); err != nil {
-		render.Render(w, r, ErrRender(err))
-		return
-	}
-}
-
-func NewTaskResponse(task *domain.Task) *TaskResponse {
-	resp := &TaskResponse{Task: task}
-
-	// if resp.User == nil {
-	// 	if user, _ := dbGetUser(resp.UserID); user != nil {
-	// 		resp.User = NewUserPayloadResponse(user)
-	// 	}
-	// }
-
-	return resp
-}
-
-func (rd *TaskResponse) Render(w http.ResponseWriter, r *http.Request) error {
-	// Pre-processing before a response is marshalled and sent across the wire
-	rd.Elapsed = 10
-	return nil
-}
+// func (rd *TaskResponse) Render(w http.ResponseWriter, r *http.Request) error {
+// 	// Pre-processing before a response is marshalled and sent across the wire
+// 	rd.Elapsed = 10
+// 	return nil
+// }
 
 // ArticleResponse is the response payload for the Article data model.
 // See NOTE above in ArticleRequest as well.
@@ -103,40 +66,40 @@ func (rd *TaskResponse) Render(w http.ResponseWriter, r *http.Request) error {
 // In the ArticleResponse object, first a Render() is called on itself,
 // then the next field, and so on, all the way down the tree.
 // Render is called in top-down order, like a http handler middleware chain.
-type TaskResponse struct {
-	*domain.Task
+// type TaskResponse struct {
+// 	*domain.Task
 
-	// User *UserPayload `json:"user,omitempty"`
+// 	// User *UserPayload `json:"user,omitempty"`
 
-	// We add an additional field to the response here.. such as this
-	// elapsed computed property
-	Elapsed int64 `json:"elapsed"`
-}
+// 	// We add an additional field to the response here.. such as this
+// 	// elapsed computed property
+// 	Elapsed int64 `json:"elapsed"`
+// }
 
-func ErrRender(err error) render.Renderer {
-	return &ErrResponse{
-		Err:            err,
-		HTTPStatusCode: 422,
-		StatusText:     "Error rendering response.",
-		ErrorText:      err.Error(),
-	}
-}
+// func ErrRender(err error) render.Renderer {
+// 	return &ErrResponse{
+// 		Err:            err,
+// 		HTTPStatusCode: 422,
+// 		StatusText:     "Error rendering response.",
+// 		ErrorText:      err.Error(),
+// 	}
+// }
 
-func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
-	render.Status(r, e.HTTPStatusCode)
-	return nil
-}
+// func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+// 	render.Status(r, e.HTTPStatusCode)
+// 	return nil
+// }
 
 // ErrResponse renderer type for handling all sorts of errors.
 //
 // In the best case scenario, the excellent github.com/pkg/errors package
 // helps reveal information on the error, setting it on Err, and in the Render()
 // method, using it to set the application-specific error code in AppCode.
-type ErrResponse struct {
-	Err            error `json:"-"` // low-level runtime error
-	HTTPStatusCode int   `json:"-"` // http response status code
+// type ErrResponse struct {
+// 	Err            error `json:"-"` // low-level runtime error
+// 	HTTPStatusCode int   `json:"-"` // http response status code
 
-	StatusText string `json:"status"`          // user-level status message
-	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
-	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
-}
+// 	StatusText string `json:"status"`          // user-level status message
+// 	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+// 	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+// }
