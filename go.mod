972def0d96e4d9e3c46fe7604ba08fca50d9637b
Dherik <dherikb@yahoo.com.br>
2024-02-14 17:44:32 -0300

    feat: sign in endpoint

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/magiconair/properties v1.8.7 // indirect
--- internal/app/handler.go
+++ internal/app/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
@@ -35,11 +36,8 @@ func SetupHandler(router *echo.Echo, service *Service) {
 	router.GET("/tasks", func(c echo.Context) error {
 		startDateParam := c.QueryParam("startDate")
 		endDateParam := c.QueryParam("endDate")
-		startDate, err := time.Parse(time.RFC3339, startDateParam)
-		if err != nil {
-			panic(err)
-		}
-		endDate, _ := time.Parse(time.RFC3339, endDateParam)
+		startDate, _ := time.Parse(time.RFC3339, startDateParam) //FIXME
+		endDate, _ := time.Parse(time.RFC3339, endDateParam)     //FIXME
 		tasks, _ := service.FindTasks(startDate, endDate)
 		return c.JSONPretty(http.StatusOK, tasks, "")
 	})
@@ -56,10 +54,75 @@ func SetupHandler(router *echo.Echo, service *Service) {
 
 		return c.JSONPretty(http.StatusCreated, nil, "")
 	})
+
+	router.POST("signin", func(c echo.Context) error {
+		creds := Credentials{}
+		if err := c.Bind(&creds); err != nil {
+			slog.Error("Error reading body", slog.String("error", err.Error()))
+			return c.JSONPretty(http.StatusBadRequest, nil, "") //FIXME
+		}
+
+		expectedPassword, ok := users[creds.Username]
+
+		if !ok || expectedPassword != creds.Password {
+			slog.Error("Password invalid")
+			return c.JSONPretty(http.StatusUnauthorized, nil, "") //FIXME
+		}
+
+		expirationTime := time.Now().Add(1 * time.Hour)
+		claims := &Claims{
+			Username: creds.Username,
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(expirationTime),
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			return c.JSONPretty(http.StatusInternalServerError, nil, "") //FIXME
+		}
+
+		signInResponse := SignInResponse{
+			Token:     tokenString,
+			ExpiresAt: expirationTime,
+			Username:  creds.Username,
+		}
+		return c.JSONPretty(http.StatusOK, signInResponse, "")
+	})
 }
 
-func searchTasks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("tasks"))
+var jwtKey = []byte("my_secret_key")
+
+// FIXME: save in the database
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+type SignInResponse struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expiresAt"`
+	// UserID       int       `json:"userId"`
+	Username     string   `json:"username"`
+	Role         string   `json:"role"`
+	RefreshToken string   `json:"refresh_token,omitempty"`
+	Metadata     Metadata `json:"metadata,omitempty"`
+}
+
+type Metadata struct {
+	Email    string `json:"email"`
+	FullName string `json:"fullName"`
 }
 
 type ErrResponse struct {
