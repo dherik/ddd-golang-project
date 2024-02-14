e3089191147b1f5d5c65d73e7aa8c44be5658b19
Dherik <dherikb@yahoo.com.br>
2024-02-14 12:10:07 -0300

    refactor: code clean up

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -25,17 +25,12 @@ func SetupHandler(router *echo.Echo, service *Service) {
 	})
 
 	router.GET("/tasks/:id", func(c echo.Context) error {
-		// tasks := []TaskResponse{
-		// 	{"1", "Task 1"},
-		// 	{"2", "Task 2"},
-		// }
 		slog.Info("Get all tasks from user")
 		tasks := service.GetTasks(c.Param("id")) //FIXME
 		return c.JSONPretty(http.StatusOK, tasks, " ")
 	})
 
 	router.POST("/tasks", func(c echo.Context) error {
-		// service.AddTask
 		slog.Info("Add new task for user")
 		t := TaskRequest{}
 		if err := c.Bind(&t); err != nil {
@@ -47,77 +42,11 @@ func SetupHandler(router *echo.Echo, service *Service) {
 
 		return c.JSONPretty(http.StatusCreated, nil, "")
 	})
-
-	// router.Route("/tasks", func(r chi.Router) {
-	// 	r.Get("/", searchTasks)
-	// 	r.Route("/{taskId}", func(r chi.Router) {
-
-	// 		r.Get("/", GetTask)
-
-	// 	})
-	// })
 }
-
-// func getTasks() []TaskResponse {
-// 	return nil
-// }
 
 func searchTasks(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("tasks"))
 }
-
-// func GetTask(w http.ResponseWriter, r *http.Request) {
-// 	// article := r.Context().Value("article").(*Article)
-
-// 	task := domain.Task{}
-
-// 	if err := render.Render(w, r, NewTaskResponse(&task)); err != nil {
-// 		render.Render(w, r, ErrRender(err))
-// 		return
-// 	}
-// }
-
-// func (rd *TaskResponse) Render(w http.ResponseWriter, r *http.Request) error {
-// 	// Pre-processing before a response is marshalled and sent across the wire
-// 	rd.Elapsed = 10
-// 	return nil
-// }
-
-// func NewTaskResponse(task *domain.Task) *TaskResponse {
-// 	resp := &TaskResponse{Task: task}
-
-// 	// if resp.User == nil {
-// 	// 	if user, _ := dbGetUser(resp.UserID); user != nil {
-// 	// 		resp.User = NewUserPayloadResponse(user)
-// 	// 	}
-// 	// }
-
-// 	return resp
-// }
-
-// type TaskResponse struct {
-// 	*domain.Task
-
-// 	// User *UserPayload `json:"user,omitempty"`
-
-// 	// We add an additional field to the response here.. such as this
-// 	// elapsed computed property
-// 	Elapsed int64 `json:"elapsed"`
-// }
-
-// func ErrRender(err error) render.Renderer {
-// 	return &ErrResponse{
-// 		Err:            err,
-// 		HTTPStatusCode: 422,
-// 		StatusText:     "Error rendering response.",
-// 		ErrorText:      err.Error(),
-// 	}
-// }
-
-// func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
-// 	render.Status(r, e.HTTPStatusCode)
-// 	return nil
-// }
 
 type ErrResponse struct {
 	Err            error `json:"-"` // low-level runtime error
