2b7b4b605c35fb0f80a00dc9ff04fe985ae11785
Dherik <dherikb@yahoo.com.br>
2024-02-19 10:16:32 -0300

    feat: set JSON output for slog. Echo log setup changed to use the same logger as slog

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,8 +1,13 @@
 package app
 
 import (
+	"context"
+	"log/slog"
+	"os"
+
 	"github.com/dherik/ddd-golang-project/internal/infrastructure/persistence"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Server struct {
@@ -17,7 +22,36 @@ func (s *Server) Start() {
 	service := NewTaskService(taskRepository)
 
 	e := echo.New()
+	setupSlog(e)
 
 	SetupHandler(e, service)
 	e.Logger.Fatal(e.Start(":3333"))
+}
+
+func setupSlog(e *echo.Echo) {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)
+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
 }
