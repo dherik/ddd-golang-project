3eaae7e5856507a4003eca20f80531e10e6ac8e5
Dherik <dherikb@yahoo.com.br>
2024-03-10 12:36:38 -0300

    test: add test for failed logins

--- tests/integration/user_test.go
+++ tests/integration/user_test.go
@@ -1,6 +1,10 @@
 package integration
 
 import (
+	"bytes"
+	"log"
+	"mime/multipart"
+	"net/http"
 	"testing"
 
 	"github.com/dherik/ddd-golang-project/tests/integration/setup"
@@ -17,7 +21,7 @@ type UserTestSuite struct {
 	suite.Suite
 }
 
-func (s *UserTestSuite) TestLogin() {
+func (s *UserTestSuite) TestLoginSuccess() {
 	if testing.Short() {
 		s.T().Skip("Skip test for postgresql repository")
 	}
@@ -27,4 +31,64 @@ func (s *UserTestSuite) TestLogin() {
 	if token == "" {
 		s.T().Fatalf("token should not be empty")
 	}
+}
+
+func (s *UserTestSuite) TestLoginUnauthorizedWhenUserNotFound() {
+	if testing.Short() {
+		s.T().Skip("Skip test for postgresql repository")
+	}
+
+	response, _ := login("non_existent_user", "some_password")
+	s.Equal(http.StatusUnauthorized, response.StatusCode)
+}
+
+func (s *UserTestSuite) TestLoginUnauthorizedWhenPasswordIsWrong() {
+	if testing.Short() {
+		s.T().Skip("Skip test for postgresql repository")
+	}
+
+	response, _ := login("admin", "some_wrong_password")
+	s.Equal(http.StatusUnauthorized, response.StatusCode)
+}
+
+func login(username, password string) (*http.Response, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// Add fields to the form
+	_ = writer.WriteField("username", username)
+	_ = writer.WriteField("password", password)
+
+	err := writer.Close()
+	if err != nil {
+		log.Fatalf("failed to close writer: %s", err.Error())
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:3333/login", body) //FIXME port as parameter
+	if err != nil {
+		log.Fatalf("failed to create the login request: %s", err.Error())
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("failed doing login request: %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	return resp, err
+
+	// if resp.StatusCode == http.StatusOK {
+	// 	var tokenResponse TokenResponse
+	// 	err := json.Unmarshal(responseBody, &tokenResponse)
+	// 	if err != nil {
+	// 		log.Fatalf("failed unmarhalling login response body: %s", err.Error())
+	// 	}
+	// 	return tokenResponse.Token
+	// }
+
+	// log.Fatalf("login failed. Status code: %d, Response: %s", resp.StatusCode, string(responseBody))
+
+	// return ""
 }
