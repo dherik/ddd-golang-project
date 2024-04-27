858e00d762698f0d46fdbf2fce29be9ccc459675
Dherik <dherikb@yahoo.com.br>
2024-04-27 15:00:52 -0300

    tests: add tests for password validation

--- internal/domain/user_test.go
+++ internal/domain/user_test.go
@@ -4,36 +4,64 @@ import (
 	"testing"
 )
 
-func TestPassword(t *testing.T) {
+func TestInvalidPassword(t *testing.T) {
+	tests := []struct {
+		name      string
+		password  string
+		wantError string
+	}{
+		{
+			name:      "empty password",
+			password:  "",
+			wantError: "password must be at least 8 characters long",
+		},
+		{
+			name:      "password less than 8 characters",
+			password:  "1234567",
+			wantError: "password must be at least 8 characters long",
+		},
+		{
+			name:      "at least one special character",
+			password:  "12345678",
+			wantError: "password must contain at least one special character",
+		},
+		{
+			name:      "password without a number",
+			password:  "!passwordwithoutnumber",
+			wantError: "password must contain at least one number",
+		},
+		{
+			name:      "password without uppercase characters",
+			password:  "!1abcdefghi",
+			wantError: "password must contain at least one uppercase letter and one lowercase letter",
+		},
+		{
+			name:      "password without lowercase characters",
+			password:  "!1ABCDEFGHI",
+			wantError: "password must contain at least one uppercase letter and one lowercase letter",
+		},
+	}
 
-	t.Run("check password length is not empty", func(t *testing.T) {
-		_, err := NewUser("username", "email@email.com", "")
-		if err == nil {
-			t.Fatalf("wanted an error but didn't get one")
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewUser("username", "username@email.com", tt.password)
+			if err == nil {
+				t.Fatalf("wanted an error but didn't get one")
+			}
 
-		want := "password must be at least 8 characters long"
-		if err.Error() != want {
-			t.Errorf("got %q, want %q", err, want)
-		}
-	})
+			if err.Error() != tt.wantError {
+				t.Errorf("got %q, want %q", err, tt.wantError)
+			}
+		})
+	}
+}
 
-	t.Run("check password length is bigger than 8 characters", func(t *testing.T) {
-		_, err := NewUser("username", "email@email.com", "1234567")
-		if err == nil {
-			t.Fatalf("wanted an error but didn't get one")
-		}
-
-		want := "password must be at least 8 characters long"
-		if err.Error() != want {
-			t.Errorf("got %q, want %q", err, want)
-		}
-	})
+func TestValidPassword(t *testing.T) {
 
 	t.Run("check password is valid", func(t *testing.T) {
 		user := User{
 			Username: "username",
-			Email:    "email@email.com",
+			Email:    "username@email.com",
 		}
 		user.SetPassword("some_password")
 
